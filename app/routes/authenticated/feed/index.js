bac9ac7f22f768c7aa9046f71a52ba752c1ed2ff
Paulo Oliveira <oliveira.phc@gmail.com>
2019-11-09 16:11:23 +0000

    Default options for service chattermill-api

--- app/routes/authenticated/feed/index.js
+++ app/routes/authenticated/feed/index.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  chattermillApi: service(),
+
+  async model() {
+    const body = await this.get('chattermillApi').fetch('/api/reviews');
+    return body.data;
+  }
 });
--- app/services/chattermill-api.js
+++ app/services/chattermill-api.js
@@ -2,6 +2,8 @@ import Service from '@ember/service';
 import RSVP from 'rsvp';
 import fetch from 'fetch';
 import { isServerErrorResponse, isAbortError } from 'ember-fetch/errors';
+import { inject as service } from '@ember/service';
+import { isEmpty } from '@ember/utils';
 
 import config from 'chattermill-test/config/environment';
 
@@ -13,9 +15,35 @@ const SUPPORT = 'We are having some problems, please contact our support team';
 const NETWORK_REQUEST_FAILED = 'Network request failed, check your connection';
 
 export default Service.extend({
-  fetch(endpoint, options) {
+  session: service(),
+
+  fetch(endpoint, options = { method: 'GET' }) {
     return new RSVP.Promise((resolve, reject) => {
-      fetch(`${apiHost}${endpoint}`, options)
+      let _options = {
+        ...options,
+      };
+
+      if (isEmpty(options.headers)) {
+        const session = this.get('session');
+        const isAuthenticated = session.get('isAuthenticated');
+        
+        if (!isAuthenticated) {
+          return reject('Not authenticated');
+        }
+
+        const data = session.get('data');
+        
+        _options = {
+          ..._options,
+          headers: {
+            'Authorization': `Bearer ${data.authenticated.token}`,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          }
+        };
+      }
+
+      fetch(`${apiHost}${endpoint}`, _options)
         .then((response) => {
           return response.json()
             .then((body) => {
